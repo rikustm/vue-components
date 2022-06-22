423717d33b0f1bc6185275b15fe7bc315037a824
Rikus Rus <rikus.rus@gmail.com>
2022-06-23 00:23:10 +0200

    update component name

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, "src/index.js"),
       name: "rikus-vue-components",
-      fileName: (format) => `my-lib.${format}.js`,
+      fileName: (format) => `rikus-vue-components.${format}.js`,
     },
     rollupOptions: {
       external: ["vue"],
